41352923675414b234084c09febe465a39bb4b98
pashutk <me@pashutk.ru>
2020-04-26 16:30:41 +0300

    Fix imagemagick command

--- server/image.ts
+++ server/image.ts
@@ -3,8 +3,7 @@ import { spawn } from "child_process";
 export const convertToBMP = (input: string, output: string): Promise<void> =>
   new Promise((resolve, reject) => {
     // magick convert -alpha off -compress none $1 BMP3:$2.bmp
-    const proc = spawn("magick", [
-      "convert",
+    const proc = spawn("convert", [
       "-alpha",
       "off",
       "-compress",
