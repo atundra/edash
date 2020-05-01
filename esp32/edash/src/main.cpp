cc5138158314c093a6f8990947d94e22fb93d670
pashutk <me@pashutk.ru>
2020-05-01 21:20:36 +0300

    Esp: load bitmap from api and draw

--- esp32/edash/src/main.cpp
+++ esp32/edash/src/main.cpp
@@ -1,19 +1,11 @@
-// mostly https://github.com/ZinggJM/GxEPD2/blob/master/examples/GxEPD2_Example/GxEPD2_Example.ino
-
 #define ENABLE_GxEPD2_GFX 0
+#include <WiFi.h>
+#include <HTTPClient.h>
 
 // #include <GxEPD2_BW.h>
 #include <GxEPD2_3C.h>
 
 GxEPD2_3C<GxEPD2_750c, GxEPD2_750c::HEIGHT> display(GxEPD2_750c(/*CS=5*/ SS, /*DC=*/17, /*RST=*/16, /*BUSY=*/4));
-
-// #include "bitmaps/Bitmaps640x384.h" // 7.5"  b/w
-#include "bitmaps.h"
-
-// const unsigned char *bitmaps[] = {Bitmap640x384_1, Bitmap640x384_2};
-const unsigned char *bitmaps[] = {Bitmap640x384_1, Bitmap640x384_2, Bitmap640x384_3};
-const uint16_t bitmapsSize = sizeof(bitmaps) / sizeof(char *);
-uint16_t currentBitmapIndex = 0;
 
 // Copy of display.drawInvertedBitmap
 void drawBitmap(int16_t x, int16_t y, const uint8_t bitmap[], int16_t w, int16_t h, uint16_t color)
@@ -41,53 +33,95 @@ void drawBitmap(int16_t x, int16_t y, const uint8_t bitmap[], int16_t w, int16_t
   }
 }
 
+char ssid[] = "ESP32NET";
+char pass[] = "PhFy9KKreeFbR4jMCzHmDDNx";
+
+int status = WL_IDLE_STATUS;
+IPAddress server(188, 226, 159, 87);
+
+WiFiClient client;
+
 void setup()
 {
   Serial.begin(115200);
-  Serial.println();
-  Serial.println("setup");
-  delay(100);
-  display.init(115200);
-  display.setFullWindow();
 
-  display.firstPage();
-  do
+  if (!WiFi.getAutoConnect() || (WiFi.getMode() != WIFI_STA) || ((WiFi.SSID() != ssid)))
   {
-    display.fillScreen(GxEPD_WHITE);
-    drawBitmap(0, 0, bitmaps[2], display.epd2.WIDTH, display.epd2.HEIGHT, GxEPD_BLACK);
-  } while (display.nextPage());
+    Serial.println();
+    Serial.print("WiFi.getAutoConnect()=");
+    Serial.println(WiFi.getAutoConnect());
+    Serial.print("WiFi.SSID()=");
+    Serial.println(WiFi.SSID());
+    WiFi.mode(WIFI_STA);
 
-  Serial.println("Power off");
-  display.powerOff();
+    Serial.print("ESP Board MAC Address:  ");
+    Serial.println(WiFi.macAddress());
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, pass);
+  }
+
+  int ConnectTimeout = 30; // 15 seconds
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+    Serial.print(WiFi.status());
+    if (--ConnectTimeout <= 0)
+    {
+      Serial.println();
+      Serial.println("WiFi connect timeout");
+      return;
+    }
+  }
+  Serial.println();
+  Serial.println("WiFi connected");
+
+  HTTPClient http;
+
+  Serial.print("[HTTP] begin...\n");
+  // configure traged server and url
+  //http.begin("https://www.howsmyssl.com/a/check", ca); //HTTPS
+  http.begin("http://bots.pashutk.ru:8000/api/image.bin"); //HTTP
+
+  Serial.print("[HTTP] GET...\n");
+  // start connection and send HTTP header
+  int httpCode = http.GET();
+
+  // httpCode will be negative on error
+  if (httpCode > 0)
+  {
+    // HTTP header has been send and Server response header has been handled
+    Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+    // file found at server
+    if (httpCode == HTTP_CODE_OK)
+    {
+      String payload = http.getString();
+      Serial.println(payload.length());
+
+      display.init(115200);
+      display.setFullWindow();
+
+      display.firstPage();
+      do
+      {
+        display.fillScreen(GxEPD_WHITE);
+        drawBitmap(0, 0, (unsigned char *)payload.c_str(), display.epd2.WIDTH, display.epd2.HEIGHT, GxEPD_BLACK);
+      } while (display.nextPage());
+
+      Serial.println("Power off");
+      display.powerOff();
+    }
+  }
+  else
+  {
+    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+
+  http.end();
 }
 
 void loop()
 {
-  return;
-
-  Serial.println("Loop start");
-  Serial.print("Draw bitmap ");
-  Serial.println(currentBitmapIndex);
-
-  Serial.println("First page start");
-  display.firstPage();
-  Serial.println("First page end");
-
-  do
-  {
-    Serial.println("Fill screen");
-    display.fillScreen(GxEPD_BLACK);
-    Serial.println("Draw inverted bitmap");
-    display.drawInvertedBitmap(0, 0, bitmaps[currentBitmapIndex], display.epd2.WIDTH, display.epd2.HEIGHT, GxEPD_WHITE);
-  } while (display.nextPage());
-
-  Serial.println("Power off");
-  display.powerOff();
-  delay(2000);
-
-  currentBitmapIndex++;
-  if (currentBitmapIndex >= bitmapsSize)
-  {
-    currentBitmapIndex = 0;
-  }
 }
