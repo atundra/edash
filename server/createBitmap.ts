0ebeeadf2ddb3b77d299e8eea249bc34f7962eb1
Roman Kuraev <ololo@desude.su>
2020-05-04 19:42:18 +0300

    use imagemagick to convert png to monochrome bitstream

--- server/createBitmap.ts
+++ server/createBitmap.ts
@@ -1,80 +1,17 @@
 import fs from 'fs';
-import { PNG } from 'pngjs';
-import { Buffer } from 'buffer';
-
-type RGBA = [number, number, number, number];
-
-const getRGBA = (data: Buffer, index: number): RGBA => [
-  data[index],
-  data[index + 1],
-  data[index + 2],
-  data[index + 4],
-];
-
-const setRGBA = (data: Buffer, index: number, rgba: RGBA): void => {
-  const [r, g, b, a] = rgba;
-  data[index] = r;
-  data[index + 1] = g;
-  data[index + 2] = b;
-  data[index + 3] = a;
-};
-
-const isBlackish = ([r, g, b]: RGBA): boolean => (r + g + b) * 0.6 < 255;
-
-const toMonochrome = (rgba: RGBA): RGBA => {
-  const threshhold = 0.6;
-  const [r, g, b, a] = rgba;
-  if ((r + g + b) * threshhold < 255) {
-    return [0, 0, 0, 255];
-  } else {
-    return [255, 255, 255, 255];
-  }
-};
-
-const formatByte = (byte: number) =>
-  byte.toString(16).padStart(2, '0').toUpperCase();
+import { convertBuffer } from './image';
 
 export const pngStreamToBitmap = (input: fs.ReadStream): Promise<Buffer> =>
   new Promise((resolve, reject) => {
-    const png = new PNG({
-      filterType: -1,
+    const buffers: Buffer[] = [];
+
+    input.on('data', (data: Buffer) => buffers.push(data));
+    input.on('end', () => {
+      convertBuffer(Buffer.concat(buffers), [
+        'PNG:-',
+        '-ordered-dither',
+        'o8x8,2',
+        'MONO:-',
+      ]).then(resolve, reject);
     });
-
-    png.on('error', (err) => reject(err));
-
-    png.on('parsed', (data) => {
-      const { height, width } = png;
-
-      const bytePerPixel = 4;
-      const pixelsPerByte = 8;
-      const loopStep = bytePerPixel * pixelsPerByte;
-
-      const out = Buffer.alloc((height * width) / pixelsPerByte);
-      let outOffset = 0;
-
-      // Iterating over blocks of 8 pixel (every pixel is 4 bytes)
-      for (
-        let blockIndex = 0;
-        blockIndex < data.length;
-        blockIndex += loopStep
-      ) {
-        let blockByte = 0;
-        for (
-          let blockPixelIndex = 0;
-          blockPixelIndex < pixelsPerByte;
-          blockPixelIndex++
-        ) {
-          const rgba = getRGBA(
-            data,
-            blockIndex + blockPixelIndex * bytePerPixel
-          );
-          const bitVal = isBlackish(rgba) ? 1 : 0;
-          blockByte |= bitVal << (pixelsPerByte - 1 - blockPixelIndex);
-        }
-        outOffset = out.writeUInt8(blockByte, outOffset);
-      }
-      resolve(out);
-    });
-
-    input.pipe(png);
   });
--- server/image.ts
+++ server/image.ts
@@ -16,6 +16,50 @@ const convertTo = (convertArgs: string[] = []): Promise<void> =>
     });
   });
 
+const reverse = (b: number) => {
+  b = ((b & 0xf0) >> 4) | ((b & 0x0f) << 4);
+  b = ((b & 0xcc) >> 2) | ((b & 0x33) << 2);
+  b = ((b & 0xaa) >> 1) | ((b & 0x55) << 1);
+  return b;
+};
+
+const toggleEndianness = function (buf: Buffer): Buffer {
+  var output = Buffer.alloc(buf.length);
+
+  for (const [i, b] of buf.entries()) {
+    output.writeUInt8(reverse(b), i);
+  }
+
+  return output;
+};
+
+export const convertBuffer = (
+  sourceBuffer: Buffer,
+  convertArgs: string[] = []
+): Promise<Buffer> =>
+  new Promise((resolve, reject) => {
+    var buffers: Buffer[] = [];
+
+    const proc = spawn('convert', convertArgs);
+
+    proc.stdout.on('data', (data) => {
+      buffers.push(data);
+    });
+
+    proc.stderr.on('data', function (data) {
+      reject(data);
+    });
+
+    proc.on('close', function (code, signal) {
+      if (code == 0 || signal == null) {
+        resolve(toggleEndianness(Buffer.concat(buffers)));
+      }
+    });
+
+    proc.stdin.write(sourceBuffer);
+    proc.stdin.end();
+  });
+
 // magick convert -alpha off -compress none $1 BMP3:$2.bmp
 export const convertToBMP = (input: string, output: string) =>
   convertTo(['-alpha', 'off', '-compress', 'none', input, `BMP3:${output}`]);
