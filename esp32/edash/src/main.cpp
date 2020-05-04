acf6fdb64dd87564daa21d0568217f9c635ea807
Timur Akhmadaliev <timur600@gmail.com>
2020-05-04 20:43:40 +0300

    rm commented code

--- esp32/edash/src/main.cpp
+++ esp32/edash/src/main.cpp
@@ -10,32 +10,6 @@ GxEPD2_3C<GxEPD2_750c, GxEPD2_750c::HEIGHT> display(GxEPD2_750c(/*CS=5*/ SS, /*D
 
 Dashboard_NS::Dashboard<GxEPD2_3C<GxEPD2_750c, GxEPD2_750c::HEIGHT>> dashboard(display);
 
-// Copy of display.drawInvertedBitmap
-// void drawBitmap(int16_t x, int16_t y, const uint8_t bitmap[], int16_t w, int16_t h, uint16_t color)
-// {
-//   Serial.println(typeid(display).name());
-//   int16_t byteWidth = (w + 7) / 8; // Bitmap scanline pad = whole byte
-//   uint8_t byte = 0;
-//   for (int16_t j = 0; j < h; j++)
-//   {
-//     for (int16_t i = 0; i < w; i++)
-//     {
-//       if (i & 7)
-//       {
-//         byte <<= 1;
-//       }
-//       else
-//       {
-//         byte = pgm_read_byte(&bitmap[j * byteWidth + i / 8]);
-//       }
-
-//       if (byte & 0x80)
-//       {
-//         display.drawPixel(x + i, y + j, color);
-//       }
-//     }
-//   }
-// }
 
 // TODO move to some class or whatever and add sending parameters
 String httpGet(const String &url)
@@ -68,26 +42,6 @@ String httpGet(const String &url)
   httpClient.end();
   return retval;
 }
-
-// void drawHttpPayload(String payload)
-// {
-//   Serial.println("Draw HTTP payload");
-//   Serial.print("Payload length: ");
-//   Serial.println(payload.length());
-
-//   display.init(115200);
-//   display.setFullWindow();
-
-//   display.firstPage();
-//   do
-//   {
-//     display.fillScreen(GxEPD_WHITE);
-//     drawBitmap(0, 0, (unsigned char *)payload.c_str(), display.epd2.WIDTH, display.epd2.HEIGHT, GxEPD_BLACK);
-//   } while (display.nextPage());
-
-//   Serial.println("Power off");
-//   display.powerOff();
-// }
 
 void setup()
 {
