e795eeb39cbb2aaff010c399d5d13fb0d202e7e4
pashutk <me@pashutk.ru>
2020-05-06 01:25:40 +0300

    Add time client tor time widget

--- esp32/edash/src/device.cpp
+++ esp32/edash/src/device.cpp
@@ -1,7 +1,9 @@
 #include "device.h"
 
 #include "GxEPD2_EPD.h"
-#include <Fonts/FreeMonoBold9pt7b.h>
+#include <Fonts/FreeSansBold18pt7b.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 
 template <typename T>
 void Dashboard_NS::Dashboard<T>::DrawBitmap(int16_t x, int16_t y, const uint8_t bitmap[], int16_t w, int16_t h, uint16_t color) const
@@ -18,6 +20,28 @@ void Dashboard_NS::Dashboard<T>::BetterDrawBitmap(const uint8_t bitmap[]) const
 template <typename T>
 void Dashboard_NS::Dashboard<T>::DrawPayload(const String &payload) const
 {
+    WiFiUDP ntpUDP;
+    NTPClient timeClient(ntpUDP);
+
+    String formattedDate;
+    String dayStamp;
+    String timeStamp;
+
+    Serial.println("Setup time client");
+    timeClient.begin();
+    const int timeOffset = 60 * 60 * 3; // GMT+3
+    Serial.printf("Setting time offset to %d seconds\n", timeOffset);
+    timeClient.setTimeOffset(60 * 60 * 3);
+
+    Serial.println("Trying to update time client");
+    while (!timeClient.update())
+    {
+        Serial.println("Time client force update");
+        timeClient.forceUpdate();
+    }
+    Serial.printf("Time client updated, day: %d, hours: %d, minutes: %d, seconds: %d\n",
+                  timeClient.getDay(), timeClient.getHours(), timeClient.getMinutes(), timeClient.getSeconds());
+
     Serial.println("Draw HTTP payload");
     Serial.print("Payload length: ");
     Serial.println(payload.length());
@@ -31,12 +55,14 @@ void Dashboard_NS::Dashboard<T>::DrawPayload(const String &payload) const
         displayDevice.fillScreen(GxEPD_WHITE);
         DrawBitmap(0, 0, (unsigned char *)payload.c_str(), displayDevice.epd2.WIDTH, displayDevice.epd2.HEIGHT, GxEPD_BLACK);
 
-        const char time[] = "04:20";
+        char time[5];
+        sprintf(time, "%02d:%02d", timeClient.getHours(), timeClient.getMinutes());
+
         const int16_t widgetX = 440;
         const int16_t widgetY = 0;
         const uint16_t widgetW = 200;
         const uint16_t widgetH = 95;
-        displayDevice.setFont(&FreeMonoBold9pt7b);
+        displayDevice.setFont(&FreeSansBold18pt7b);
         displayDevice.setTextColor(GxEPD_BLACK);
         int16_t tbx, tby;
         uint16_t tbw, tbh;
