f39b834e7e735c145900ac0d05132aad60881389
Pavel Glushkov <me@pashutk.ru>
2020-05-20 00:08:12 +0300

    Refoc put handler (#17)
    * Refoc put handler
    
    * Another refock
    
    * Handler error class
    
    * Get handler refock
    
    * Hyper-ts
    
    * Revert platformio.ini
    
    * Move put to hyper-ts
    
    * Little cleanup

--- server/api/configurationRouter.ts
+++ server/api/configurationRouter.ts
@@ -1,10 +1,21 @@
-import { Router, RequestHandler } from 'express';
+import { Router } from 'express';
 import { readFile, mkdirSync, accessSync, writeFile } from 'fs';
 import { promisify } from 'util';
 import { PathReporter } from 'io-ts/lib/PathReporter';
-import { isLeft } from 'fp-ts/lib/Either';
+import * as E from 'fp-ts/lib/Either';
+import * as TE from 'fp-ts/lib/TaskEither';
+import * as H from 'hyper-ts/lib/index';
+import * as HE from 'hyper-ts/lib/express';
+import { NonEmptyString } from 'io-ts-types/lib/NonEmptyString';
 
 import { validateWidgetConfig } from '../validation';
+import { pipe } from 'fp-ts/lib/pipeable';
+import { Errors } from 'io-ts';
+import { WidgetConfig } from '../renderer/types';
+
+class HandlerError {
+  constructor(public code: H.Status, public message: string) {}
+}
 
 const readFileP = promisify(readFile);
 const writeFileP = promisify(writeFile);
@@ -20,45 +31,66 @@ try {
 const getConfiguration = (id: string) => readFileP(`${CONFIGURATION_PATH}/${id}.json`);
 const updateConfiguration = (id: string, data: string) => writeFileP(`${CONFIGURATION_PATH}/${id}.json`, data);
 
-const getHandler: RequestHandler = async (req, res) => {
-  const { id } = req.params;
+const getRequiredParamError = <T>(name: T) => new HandlerError(400, `Query param ${name} is required`);
 
-  if (!id) {
-    res.sendStatus(400);
-  }
+const getConfig = (id: string): TE.TaskEither<HandlerError, Buffer> =>
+  TE.tryCatch(
+    () => getConfiguration(id),
+    // Shitty node js typings for Promise meh
+    (reason) =>
+      (reason as NodeJS.ErrnoException).code === 'ENOENT'
+        ? new HandlerError(404, `Config with id ${id} not found`)
+        : new HandlerError(500, `Can't get config with id ${id}`)
+  );
 
-  try {
-    res.send(await getConfiguration(id));
-  } catch (e) {
-    if (e.code === 'ENOENT') {
-      return res.sendStatus(404);
-    }
+const setConfig = (id: string, config: WidgetConfig): TE.TaskEither<HandlerError, void> =>
+  TE.tryCatch(
+    () => updateConfiguration(id, JSON.stringify(config)),
+    (reason) => new HandlerError(500, String(reason))
+  );
 
-    res.sendStatus(500);
-  }
-};
+const validationErrorsToHandlerError = (errors: Errors) =>
+  new HandlerError(400, `WidgetConfig validate errors:\n${PathReporter.report(E.left(errors)).join('\n')}`);
 
-const putHandler: RequestHandler = async (req, res) => {
-  const { id } = req.params;
+const sendConfig = (buffer: Buffer): H.Middleware<H.StatusOpen, H.ResponseEnded, HandlerError, void> =>
+  pipe(
+    H.status(H.Status.OK),
+    H.ichain(H.closeHeaders),
+    H.ichain(() => H.modifyConnection((c) => c.setBody(buffer)))
+  );
 
-  if (!id) {
-    res.sendStatus(400);
-  }
+const sendOk = (): H.Middleware<H.StatusOpen, H.ResponseEnded, HandlerError, void> =>
+  pipe(H.status(200), H.ichain(H.closeHeaders), H.ichain(H.end));
 
-  const validationResult = validateWidgetConfig(req.body);
+const sendError = (err: HandlerError): H.Middleware<H.StatusOpen, H.ResponseEnded, never, void> =>
+  pipe(
+    H.status(err.code),
+    H.ichain(H.closeHeaders),
+    H.ichain(() => H.send(err.message))
+  );
 
-  if (isLeft(validationResult)) {
-    return res.status(400).send(PathReporter.report(validationResult));
-  }
+const put: H.Middleware<H.StatusOpen, H.ResponseEnded, HandlerError, void> = pipe(
+  H.decodeBody(validateWidgetConfig),
+  H.mapLeft(validationErrorsToHandlerError),
+  H.ichain((config) =>
+    pipe(
+      H.decodeParam('id', NonEmptyString.decode),
+      H.mapLeft(() => getRequiredParamError('id')),
+      H.ichain((id) => H.fromTaskEither(setConfig(id, config)))
+    )
+  ),
+  H.ichain(sendOk),
+  H.orElse(sendError)
+);
 
-  try {
-    await updateConfiguration(id, JSON.stringify(validationResult.right));
-    res.sendStatus(200);
-  } catch (e) {
-    res.sendStatus(500);
-  }
-};
+const get: H.Middleware<H.StatusOpen, H.ResponseEnded, HandlerError, void> = pipe(
+  H.decodeParam('id', NonEmptyString.decode),
+  H.mapLeft(() => getRequiredParamError('id')),
+  H.ichain((id) => H.fromTaskEither(getConfig(id))),
+  H.ichain(sendConfig),
+  H.orElse(sendError)
+);
 
-const router = Router().get('/:id', getHandler).put('/:id', putHandler);
+const router = Router().get('/:id', HE.toRequestHandler(get)).put('/:id', HE.toRequestHandler(put));
 
 export default router;
