9cddb09ef34cf042e66a63a47013259733cd4ff4
pashutk <me@pashutk.ru>
2020-05-30 17:52:22 +0300

    Fix async server starting flow

--- server/express.ts
+++ server/express.ts
@@ -12,6 +12,10 @@ import { pipe } from 'fp-ts/lib/pipeable';
 import { getPassportMiddleware } from './passport';
 import { router as apiRouter } from './api';
 import type { Config } from './config';
+import http, { Server } from 'http';
+
+const v = http.createServer(() => {});
+const b = v.listen(12, () => {});
 
 const use: ApplicationRequestHandler<(app: Application) => Application> = <A extends any[]>(...args: A) => (
   app: Application
@@ -42,4 +46,13 @@ export const createServer = (mongoClient: MongoClient) => (
         (err) => err as NodeJS.ErrnoException
       ),
     TE.fromEither
+  );
+
+export const listen = (server: Application, port: string | number): TE.TaskEither<Error, Application> =>
+  TE.tryCatch(
+    () =>
+      new Promise((resolve, reject) => {
+        server.listen(port, () => resolve(server));
+      }),
+    (reason) => reason as Error
   );
--- server/index.ts
+++ server/index.ts
@@ -5,13 +5,13 @@ import type { Config } from './config';
 import { isDev } from './config/env';
 import { createMongoClient } from './db';
 import * as TE from 'fp-ts/lib/TaskEither';
+import * as RT from 'fp-ts/lib/ReaderTask';
 import * as RTE from 'fp-ts/lib/ReaderTaskEither';
-import * as E from 'fp-ts/lib/Either';
+import * as IOE from 'fp-ts/lib/IOEither';
 import { pipe } from 'fp-ts/lib/pipeable';
-import { log } from 'fp-ts/lib/Console';
+import { log, error } from 'fp-ts/lib/Console';
 import NextServer from 'next/dist/next-server/server/next-server';
-import { Server } from 'http';
-import { createServer as createExpressServer } from './express';
+import { createServer as createExpressServer, listen as expressListen } from './express';
 
 const runNextServer = RTE.rightReaderTask<Config, never, NextServer>(({ ENV }) => async () => {
   const server = next({ dev: isDev(ENV) });
@@ -21,21 +21,23 @@ const runNextServer = RTE.rightReaderTask<Config, never, NextServer>(({ ENV }) =
 
 const expressServerListen = (
   expressServer: Application
-): RTE.ReaderTaskEither<Config, NodeJS.ErrnoException, Server> => ({ PORT }) =>
-  TE.fromEither(
-    E.tryCatch(
-      () => expressServer.listen(PORT, log(`Http server started on ${PORT} port`)),
-      (e) => e as NodeJS.ErrnoException
-    )
+): RTE.ReaderTaskEither<Config, NodeJS.ErrnoException, Application> => ({ PORT }) =>
+  pipe(
+    expressListen(expressServer, PORT),
+    TE.chainFirst(() => TE.fromIOEither(IOE.rightIO(log(`Http server started on ${PORT} port`))))
   );
 
-RTE.run(
+RT.run(
   pipe(
     RTE.ask<Config>(),
     RTE.chain(({ MONGO_CONNECTION_URI }) => RTE.fromTaskEither(createMongoClient(MONGO_CONNECTION_URI))),
     RTE.mapLeft((mongoErr) => new Error(mongoErr.message)),
     RTE.chain((mongoClient) => pipe(runNextServer, RTE.chain(createExpressServer(mongoClient)))),
-    RTE.chain(expressServerListen)
+    RTE.chain(expressServerListen),
+    RTE.fold(
+      (err) => RT.fromIO(error(err)),
+      (server) => RT.fromIO(log('Server started'))
+    )
   ),
   CONFIG
 );
