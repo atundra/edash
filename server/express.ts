d91e0607ba8ed14093013dd88aa7489cd1bee58f
pashutk <me@pashutk.ru>
2020-05-30 13:40:59 +0300

    Passport related express server configuration

---
+++ server/express.ts
@@ -0,0 +1,47 @@
+import { parse } from 'url';
+import express, { Application } from 'express';
+import { ApplicationRequestHandler } from 'express-serve-static-core';
+import session from 'express-session';
+import bodyParser from 'body-parser';
+import { MongoClient } from 'mongodb';
+import NextServer from 'next/dist/next-server/server/next-server';
+import * as E from 'fp-ts/lib/Either';
+import * as TE from 'fp-ts/lib/TaskEither';
+import * as RTE from 'fp-ts/lib/ReaderTaskEither';
+import { pipe } from 'fp-ts/lib/pipeable';
+import { getPassportMiddleware } from './passport';
+import { router as apiRouter } from './api';
+import * as CONFIG from './config';
+
+type Config = typeof CONFIG;
+
+const use: ApplicationRequestHandler<(app: Application) => Application> = <A extends any[]>(...args: A) => (
+  app: Application
+) => app.use(...args);
+
+export const createServer = (mongoClient: MongoClient) => (
+  nextServer: NextServer
+): RTE.ReaderTaskEither<Config, NodeJS.ErrnoException, Application> => ({ EXPRESS_SESSION_SECRET }) =>
+  pipe(
+    getPassportMiddleware(),
+    (passportInstance) =>
+      E.tryCatch(
+        () =>
+          pipe(
+            express(),
+            use(session({ secret: EXPRESS_SESSION_SECRET, resave: false, saveUninitialized: false })),
+            use(bodyParser.urlencoded({ extended: false })),
+            use(passportInstance.initialize()),
+            use(passportInstance.session()),
+            use('/api', apiRouter),
+            (app) =>
+              app.all('*', (req, res, next) => {
+                const parsed = parse(req.url, true);
+                const handler = nextServer.getRequestHandler();
+                return handler(req, res, parsed);
+              })
+          ),
+        (err) => err as NodeJS.ErrnoException
+      ),
+    TE.fromEither
+  );
--- server/passport.ts
+++ server/passport.ts
@@ -7,10 +7,24 @@ import type { VerifyCallback as Oauth2VerifyCallback } from 'passport-oauth2';
 import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, GITHUB_CALLBACK_URL } from './config';
 import passport, { Profile } from 'passport';
 import TE, { TaskEither } from 'fp-ts/lib/TaskEither';
+import { pipe } from 'fp-ts/lib/pipeable';
 // import T from 'fp-ts/lib/Task';
 // import { left, right } from 'fp-ts/lib/Either';
 
 export const passportInstance = passport;
+
+const retFirst = <A>(work: (a: A) => unknown) => (a: A): A => {
+  work(a);
+  return a;
+};
+
+export const getPassportMiddleware = () =>
+  pipe(
+    passport,
+    (p) => p.use(getGithubStrategy()),
+    retFirst((p) => p.serializeUser((user, done) => {})),
+    retFirst((p) => p.deserializeUser((id, done) => {}))
+  );
 
 const getGithubStrategyConfig = (): GithubStrategyOptions => ({
   clientID: GITHUB_CLIENT_ID,
