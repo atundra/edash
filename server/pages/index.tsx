9a2ce1f312b141c84fc25e3915227bb2b8ae1b3d
pashutk <me@pashutk.ru>
2020-05-31 16:46:34 +0300

    Dummy index page

--- server/pages/index.tsx
+++ server/pages/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import styles from './index.module.css';
 
 const Index = () => {
-  return <div>Index</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.body}>
+        <div className={styles.screen}>
+          <div className={styles.text}>
+            <h1 className={styles.header}>eDash</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Index;
